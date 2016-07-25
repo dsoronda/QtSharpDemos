48f1c0961f25d2d65ac079dcfc24b0de1c04d357
Dražen Šoronda <dsoronda@gmail.com>
2016-07-25 00:00:22 -0700

    Init

---
+++ source/QtSharpDemos/GuiExample/QtWidgetDemo1.cs
@@ -0,0 +1,38 @@
+﻿using QtWidgets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QtSharpDemos.GuiExample
+{
+    class QtWidgetDemo1 : QtWidgets.QWidget
+    {
+        const int WIDTH = 250;
+        const int HEIGHT = 150;
+
+        public QtWidgetDemo1()
+        {
+            WindowTitle = "TEST";
+            ToolTip = "This is QT Qwidget";
+            Resize(250, 150);
+            Center();
+            Show();
+        }
+
+        private void Center()
+        {
+            var qdw = new QDesktopWidget();
+
+            int screenWidth = qdw.Width;
+            int screenHeight = qdw.Height;
+
+            int cx = (screenWidth - WIDTH) / 2;
+            int cy = (screenHeight - HEIGHT) / 2;
+
+            Move(cx, cy);
+        }
+    }
+
+}
---
+++ source/QtSharpDemos/Program.cs
@@ -0,0 +1,38 @@
+﻿using QtWidgets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QtSharpDemos.GuiExample;
+
+namespace QtSharpDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Init QT application
+            int count = 0;
+            // project-> properties -> allow unsafe code (must ce checked for now)
+            try
+            {
+                // TODO : added try/catch block so we can log exception errors
+                unsafe
+                {
+                    var qtApp = new QApplication(ref count, null);
+                }
+                var w = new QtWidgetDemo1();
+                QApplication.Exec();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            Console.WriteLine("All done");
+            Console.ReadKey();
+
+        }
+    }
+}
