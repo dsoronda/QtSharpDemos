673a898b421e00370bec50eca2901f8b4b446925
Dražen Šoronda <dsoronda@outlook.com>
2016-07-27 10:44:59 +0200

    Codecleanup Paint demo

--- source/QtSharpDemos/GuiExample/PaintDemo.cs
+++ source/QtSharpDemos/GuiExample/PaintDemo.cs
@@ -17,14 +17,35 @@ namespace QtSharpDemos.GuiExample
         {
             WindowTitle = "Paint Demo";
 
-            var paintEvent = new QPaintEvent(this.Rect);
-            // HOW TO ADD ACTION TO PAINT EVENT ????
-            this.OnPaintEvent(paintEvent);
+            //this.OnPaintEvent(paintEvent); // ??
             //this.OnPaintEvent += MyPaintEvent; // we need something like this ?
 
             Resize(250, 150);
             Move(300, 300);
             Show();
+        }
+
+        protected override void OnPaintEvent(QPaintEvent e)
+        {
+            base.OnPaintEvent(e);
+
+            // this is example from Qt site, http://doc.qt.io/qt-5/qtwidgets-widgets-scribble-example.html
+            // next line throws Access Violation exception, "Attempted to read or write protected memory. This is often an indication that other memory is corrupt."}
+            var painter = new QPainter(this);// throws Access Violation exception
+
+            // this.InitPainter(painter);    // throws Access Violation exception , same as abowe, if commented nothing is drawn on widget
+
+            DrawPatterns(painter);
+
+            painter.End();
+        }
+
+        void DrawPatterns(QPainter ptr)
+        {
+            ptr.SetPen(PenStyle.NoPen);
+
+            ptr.SetBrush(BrushStyle.HorPattern);
+            ptr.DrawRect(10, 15, 90, 60);
         }
 
         protected void MyPaintEvent(QPaintEvent e)
@@ -36,24 +57,8 @@ namespace QtSharpDemos.GuiExample
             painter.End();
         }
 
-        protected override void OnPaintEvent(QPaintEvent e)
-        {
-            base.OnPaintEvent(e);
 
-            var painter = new QPainter();
-
-            // this is example from Qt site, http://doc.qt.io/qt-5/qtwidgets-widgets-scribble-example.html
-            // throws Access Violation exception, "Attempted to read or write protected memory. This is often an indication that other memory is corrupt."}
-            //var painter2 = new QPainter(this);// throws Access Violation exception
-
-            // this.InitPainter(painter);    // throws Access Violation exception , same as abowe, if commented nothing is drawn on widget
-
-            DrawPatterns(painter);
-
-            painter.End();
-        }
-
-        void DrawPatterns(QPainter ptr)
+        void DrawPatternsEx(QPainter ptr)
         {
             ptr.SetPen(PenStyle.NoPen);
 
@@ -84,6 +89,5 @@ namespace QtSharpDemos.GuiExample
             ptr.SetBrush(BrushStyle.DiagCrossPattern);
             ptr.DrawRect(250, 195, 90, 60);
         }
-
     }
 }
